b770fdc5fc56d12ba248892d056c43690035980f
VerbalCheats <101001475+VerbalCheats@users.noreply.github.com>
2022-03-10 21:36:54 +0000

    Files

---
+++ boredasfuck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace boredasfuck
+{
+    /*
+        Random AC Health Hack
+        I was rlly bored :)    
+    */
+    internal class Program
+    {
+        public static int Base = 0x004E4DBC;
+        public static int Health = 0xF4;
+
+        static void Main(string[] args)
+        {
+            VAMemory memory = new VAMemory("ac_client");
+
+            int LocalPlayer = memory.ReadInt32((IntPtr)Base);
+
+            while(true)
+            {
+                int address = LocalPlayer + Health;
+                memory.WriteInt32((IntPtr)address, 69420);
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
